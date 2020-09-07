93a9b8cb253e68fefee3b65e9f21c645be43e044
Igor Bernardo Costa <igorbernardocosta@gmail.com>
2020-09-06 23:03:13 -0300

    Includes Drag and Drop File

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-function helloWorld() {
-    for (let i = 3; i > 0; i--) { console.log(i); }
-    console.log('...');
-    console.log('Hello World!');
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        var fileName;
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+            if (ev.dataTransfer.items[i].kind === 'file') {
+                var file = ev.dataTransfer.items[i].getAsFile();
+                console.log('... file[' + i + '].name = ' + file.name);
+                fileName = file.name;
+                console.log(fileName);
+                var elementHtml = document.createElement('li');
+                elementHtml.innerHTML = fileName
+                document.getElementById('filesList').appendChild(elementHtml);
+            }
+        }
+    } else {
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+        }
+    }
 }
-helloWorld();
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+
+    ev.preventDefault();
+}
